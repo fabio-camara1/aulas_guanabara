3f3d5663b19dfc70660469fc8b912a20c845ef8b
fabio camara <128440879+fabio-camara1@users.noreply.github.com>
2023-05-01 21:12:35 -0300

    Add files via upload

---
+++ desafio_4_guanabara.py
@@ -0,0 +1,5 @@
+n = int(input("digite um numero:"))
+d = n * 2
+t = n * 3
+r = n ** (1 / 2)
+print("o dobro de {} é {} \no triplo de {} é {}\ne a raiz quadrada de {} é {:.2f}".format(n, d, n, t, n, r))
