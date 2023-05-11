2fda567b21d4008538c049f9cbf60d192c3dcd7e
fabio camara <128440879+fabio-camara1@users.noreply.github.com>
2023-05-10 23:55:14 -0300

    Add files via upload

---
+++ desafio12_guanabara.py
@@ -0,0 +1,3 @@
+celsius = int(input("digite aqui a temperatura em graus celsius:"))
+fahrenheit = (celsius * 1.8) + 32
+print("a conversão de {}°C para fahrenheit é {:.0f}F°".format(celsius, fahrenheit))
